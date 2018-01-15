bccf6373cb23bf1c7c5d5f1516e0f0a367242842
Eugene Nosko <coder@venux.com>
2018-01-15 09:04:51 -0500

    OpenVG

--- src/openvg/openvg.js
+++ src/openvg/openvg.js
@@ -1,64 +1,148 @@
-"use strict"
+"use strict";
 
-var wcwidth = require('../')
-var test = require('tape')
+// Some environments don't have global Buffer (e.g. React Native).
+// Solution would be installing npm modules "buffer" and "stream" explicitly.
+var Buffer = require("buffer").Buffer;
 
-test('handles regular strings', function(t) {
-  t.strictEqual(wcwidth('abc'), 3)
-  t.end()
-})
+var bomHandling = require("./bom-handling"),
+    iconv = module.exports;
 
-test('handles multibyte strings', function(t) {
-  t.strictEqual(wcwidth('字的模块'), 8)
-  t.end()
-})
+// All codecs and aliases are kept here, keyed by encoding name/alias.
+// They are lazy loaded in `iconv.getCodec` from `encodings/index.js`.
+iconv.encodings = null;
 
-test('handles multibyte characters mixed with regular characters', function(t) {
-  t.strictEqual(wcwidth('abc 字的模块'), 12)
-  t.end()
-})
+// Characters emitted in case of error.
+iconv.defaultCharUnicode = '�';
+iconv.defaultCharSingleByte = '?';
 
-test('ignores control characters e.g. \\n', function(t) {
-  t.strictEqual(wcwidth('abc\n字的模块\ndef'), 14)
-  t.end()
-})
+// Public API.
+iconv.encode = function encode(str, encoding, options) {
+    str = "" + (str || ""); // Ensure string.
 
-test('ignores bad input', function(t) {
-  t.strictEqual(wcwidth(''), 0)
-  t.strictEqual(wcwidth(3), 0)
-  t.strictEqual(wcwidth({}), 0)
-  t.strictEqual(wcwidth([]), 0)
-  t.strictEqual(wcwidth(), 0)
-  t.end()
-})
+    var encoder = iconv.getEncoder(encoding, options);
 
-test('ignores nul (charcode 0)', function(t) {
-  t.strictEqual(wcwidth(String.fromCharCode(0)), 0)
-  t.end()
-})
+    var res = encoder.write(str);
+    var trail = encoder.end();
+    
+    return (trail && trail.length > 0) ? Buffer.concat([res, trail]) : res;
+}
 
-test('ignores nul mixed with chars', function(t) {
-  t.strictEqual(wcwidth('a' + String.fromCharCode(0) + '\n字的'), 5)
-  t.end()
-})
+iconv.decode = function decode(buf, encoding, options) {
+    if (typeof buf === 'string') {
+        if (!iconv.skipDecodeWarning) {
+            console.error('Iconv-lite warning: decode()-ing strings is deprecated. Refer to https://github.com/ashtuchkin/iconv-lite/wiki/Use-Buffers-when-decoding');
+            iconv.skipDecodeWarning = true;
+        }
 
-test('can have custom value for nul', function(t) {
-  t.strictEqual(wcwidth.config({
-    nul: 10
-  })(String.fromCharCode(0) + 'a字的'), 15)
-  t.end()
-})
+        buf = new Buffer("" + (buf || ""), "binary"); // Ensure buffer.
+    }
 
-test('can have custom control char value', function(t) {
-  t.strictEqual(wcwidth.config({
-    control: 1
-  })('abc\n字的模块\ndef'), 16)
-  t.end()
-})
+    var decoder = iconv.getDecoder(encoding, options);
 
-test('negative custom control chars == -1', function(t) {
-  t.strictEqual(wcwidth.config({
-    control: -1
-  })('abc\n字的模块\ndef'), -1)
-  t.end()
-})
+    var res = decoder.write(buf);
+    var trail = decoder.end();
+
+    return trail ? (res + trail) : res;
+}
+
+iconv.encodingExists = function encodingExists(enc) {
+    try {
+        iconv.getCodec(enc);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+// Legacy aliases to convert functions
+iconv.toEncoding = iconv.encode;
+iconv.fromEncoding = iconv.decode;
+
+// Search for a codec in iconv.encodings. Cache codec data in iconv._codecDataCache.
+iconv._codecDataCache = {};
+iconv.getCodec = function getCodec(encoding) {
+    if (!iconv.encodings)
+        iconv.encodings = require("../encodings"); // Lazy load all encoding definitions.
+    
+    // Canonicalize encoding name: strip all non-alphanumeric chars and appended year.
+    var enc = (''+encoding).toLowerCase().replace(/[^0-9a-z]|:\d{4}$/g, "");
+
+    // Traverse iconv.encodings to find actual codec.
+    var codecOptions = {};
+    while (true) {
+        var codec = iconv._codecDataCache[enc];
+        if (codec)
+            return codec;
+
+        var codecDef = iconv.encodings[enc];
+
+        switch (typeof codecDef) {
+            case "string": // Direct alias to other encoding.
+                enc = codecDef;
+                break;
+
+            case "object": // Alias with options. Can be layered.
+                for (var key in codecDef)
+                    codecOptions[key] = codecDef[key];
+
+                if (!codecOptions.encodingName)
+                    codecOptions.encodingName = enc;
+                
+                enc = codecDef.type;
+                break;
+
+            case "function": // Codec itself.
+                if (!codecOptions.encodingName)
+                    codecOptions.encodingName = enc;
+
+                // The codec function must load all tables and return object with .encoder and .decoder methods.
+                // It'll be called only once (for each different options object).
+                codec = new codecDef(codecOptions, iconv);
+
+                iconv._codecDataCache[codecOptions.encodingName] = codec; // Save it to be reused later.
+                return codec;
+
+            default:
+                throw new Error("Encoding not recognized: '" + encoding + "' (searched as: '"+enc+"')");
+        }
+    }
+}
+
+iconv.getEncoder = function getEncoder(encoding, options) {
+    var codec = iconv.getCodec(encoding),
+        encoder = new codec.encoder(options, codec);
+
+    if (codec.bomAware && options && options.addBOM)
+        encoder = new bomHandling.PrependBOM(encoder, options);
+
+    return encoder;
+}
+
+iconv.getDecoder = function getDecoder(encoding, options) {
+    var codec = iconv.getCodec(encoding),
+        decoder = new codec.decoder(options, codec);
+
+    if (codec.bomAware && !(options && options.stripBOM === false))
+        decoder = new bomHandling.StripBOM(decoder, options);
+
+    return decoder;
+}
+
+
+// Load extensions in Node. All of them are omitted in Browserify build via 'browser' field in package.json.
+var nodeVer = typeof process !== 'undefined' && process.versions && process.versions.node;
+if (nodeVer) {
+
+    // Load streaming support in Node v0.10+
+    var nodeVerArr = nodeVer.split(".").map(Number);
+    if (nodeVerArr[0] > 0 || nodeVerArr[1] >= 10) {
+        require("./streams")(iconv);
+    }
+
+    // Load Node primitive extensions.
+    require("./extend-node")(iconv);
+}
+
+if ("Ā" != "\u0100") {
+    console.error("iconv-lite warning: javascript files use encoding different from utf-8. See https://github.com/ashtuchkin/iconv-lite/wiki/Javascript-source-file-encodings for more info.");
+}
